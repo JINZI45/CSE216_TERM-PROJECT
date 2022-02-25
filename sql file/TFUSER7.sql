80964d091d9ff47251ceea7ae945d136cca9865e
JINZI45 <swarnalisaha311220@gmail.com>
2022-02-25 23:31:58 +0600

    sql file is added

---
+++ sql file/TFUSER7.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : demo
+ Source Server Type    : Oracle
+ Source Server Version : 120200
+ Source Host           : 103.94.135.201:1521
+ Source Schema         : TFUSER7
+
+ Target Server Type    : Oracle
+ Target Server Version : 120200
+ File Encoding         : 65001
+
+ Date: 25/02/2022 22:58:51
+*/
+
